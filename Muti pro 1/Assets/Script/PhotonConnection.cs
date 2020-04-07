f5dde6b7430c96a460e382088d4cbc6a39d740fc
Naruebase <63284083+Naruebase@users.noreply.github.com>
2020-04-07 16:12:22 +0700

    Test Pun

---
+++ Muti pro 1/Assets/Script/PhotonConnection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonConnection : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    IEnumerable Connect()
+    {
+        yield return null;
+    }
+}
